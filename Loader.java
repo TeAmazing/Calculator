94b442ec281e55d19fb6a4afda08fd1f42604c8f
Kaigh <krazykaigh@gmail.com>
2014-11-22 17:33:57 -0500

    Loader Update 2014.11.22@17:32
    This update allows me to determine if the save folder is present, if not it is created.
    All of this code will be shifted as this method should only upload the file and then parse out the information and send it back to the control file.
    Some of this will be moved to a new Saver backing bean.

--- Loader.java
+++ Loader.java
@@ -1,3 +1,8 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.teamazing.backingbeans;
 
 import java.io.File;
@@ -6,9 +11,13 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
+import javax.faces.bean.RequestScoped;
 import javax.servlet.http.Part;
 
 /**
@@ -16,34 +25,58 @@ import javax.servlet.http.Part;
  * @javatutorials.com
  * @author javatutorials
  * Utilized by TeAmazing as base for uploading tabulations
+ * 
+ * This file will be changed. Currently, it uploads a file and then
+ * saves that file in a directory named c:\calcTab
+ * 
+ * After the upload, the file will need to be parsed into type InputLine
+ * and sent back tot index.xhtml
+ * 
  */
 
 @ManagedBean(name = "loader")
-@ViewScoped
+@RequestScoped
 
-public class Loader implements Serializable {
+public class Loader implements Serializable 
+{
 
     private static final long serialVersionUID = 9040359120893077422L;
     private static final int DEFAULT_BUFFER_SIZE = 10240; // 10KB.
 
     private Part part;
     private String statusMessage;
+    private String fileDate;
+    private final ArrayList<InputLine> uploadLines = new ArrayList<InputLine>();
+   
+    private DateFormat dateFormat;  
+    private Date date;
 
+    
     public String uploadFile() throws IOException 
     {
         // Extract file name from content-disposition header of file part
         String fileName = getFileName(part);
         System.out.println("***** fileName: " + fileName);
-
-        String basePath = "C:" + File.separator + "temp" + File.separator;
-        File outputFilePath = new File(basePath + fileName);
+        
+        // Add the timedate stamp to the file name
+        dateFormat = new SimpleDateFormat("yyyy.MM.dd_HH.mm_");
+        date = new Date();
+        fileDate = dateFormat.format(date);
+        
+        // Setup the file to receive the download
+        String basePath = "C:" + File.separator + "calcTab"; 
+        // Determine if path (Directory) exists, if not create
+        createDirectoryIfNeeded(basePath);
+        //complete file name
+        basePath += File.separator;
+        File outputFilePath = new File(basePath + fileDate + fileName);
 
         // Copy uploaded file to destination path
-        InputStream inputStream = null;
+        InputStream inputStream = null; // This creates the input stream
         OutputStream outputStream = null;
         try 
         {
-                inputStream = part.getInputStream();
+                inputStream = part.getInputStream(); // This fills the input stream
                 outputStream = new FileOutputStream(outputFilePath);
 
                 int read = 0;
@@ -70,43 +103,57 @@ public class Loader implements Serializable {
                 {
                         inputStream.close();
                 }
+                part.delete();  //Clear this from the stream
         }
         return "index";    // return to calculator page
     }
 
     public Part getPart() 
     {
-            return part;
+        return part;
     }
 
     public void setPart(Part part) 
     {
-            this.part = part;
+        this.part = part;
     }
 
     public String getStatusMessage() 
     {
-            return statusMessage;
+        return statusMessage;
     }
 
     public void setStatusMessage(String statusMessage) 
     {
-            this.statusMessage = statusMessage;
+        this.statusMessage = statusMessage;
     }
 
+    private void createDirectoryIfNeeded(String directoryName)
+    {
+        File theDir = new File(directoryName);
+        System.out.println("@@@@@@ checking directory: " + directoryName);
+
+        // if the directory does not exist, create it
+        if (!theDir.exists())
+        {
+          System.out.println("@@@@@@ creating directory: " + directoryName);
+          theDir.mkdir();
+        }
+    }
     // Extract file name from content-disposition header of file part
     private String getFileName(Part part) 
     {
-            final String partHeader = part.getHeader("content-disposition");
-            System.out.println("***** partHeader: " + partHeader);
-            for (String content : part.getHeader("content-disposition").split(";")) 
-            {
-                    if (content.trim().startsWith("filename")) 
-                    {
-                            return content.substring(content.indexOf('=') + 1).trim()
-                                            .replace("\"", "");
-                    }
-            }
-            return null;
+        final String partHeader = part.getHeader("content-disposition");
+        System.out.println("***** partHeader: " + partHeader);
+        for (String content : part.getHeader("content-disposition").split(";")) 
+        {
+                if (content.trim().startsWith("filename")) 
+                {
+                        return content.substring(content.indexOf('=') + 1).trim()
+                                        .replace("\"", "");
+                }
+        }
+        return null;
     }
 }
+   
